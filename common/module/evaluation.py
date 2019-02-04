d60d00823f79b237938dd8b61a0ef79836983ea7
esaki01 <social0430@gmail.com>
2019-02-04 23:34:17 +0900

    [Minor] Modify evaluate score method

--- common/module/evaluation.py
+++ common/module/evaluation.py
@@ -1,19 +1,18 @@
 from sklearn.metrics import f1_score
-from termcolor import cprint
+from sklearn.metrics import r2_score
 
 
-def score(predict, target):
-    """Evaluate accuracy by using f1 score.
+def score(predict, target, type='f1'):
+    """Evaluate accuracy.
     
     predict: predict lables
     target: target labels
     """
-    score = f1_score(target.values.astype(int), predict.values.astype(float), average='micro')
-
-    passing_score = 0.80
-    if score > passing_score:
-        is_passed = True
+    if type is 'f1':
+        score = f1_score(target.values.astype(int), predict.values.astype(float), average='micro')
+    elif type is 'r2':
+        score = r2_score(target.values, predict.values)
     else:
-        is_passed = False
+        raise ValueError('the score type is wrong. please choice f1 or r2.')
 
-    cprint('result: {}\nscore: {}'.format(is_passed, score), 'red', attrs=['bold'])
+    return score
