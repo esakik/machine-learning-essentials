d3c65f295bb789c416d4c43f5639c63e99c1e75d
esaki01 <social0430@gmail.com>
2019-02-11 16:09:03 +0900

    [Enhancement] Implement decorrelate and whitening

--- common/module/preprocessing.py
+++ common/module/preprocessing.py
@@ -1,9 +1,39 @@
 import numpy as np
 
 
-def standardize(x):
+def standardize(X):
     """standardize data.
 
-    x: input data
+    X: input data
     """
-    return (x - np.mean(x, axis=0)) / np.std(x, axis=0)
+    return (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+
+
+def decorrelate(X):
+    """decorrelate data.
+    
+    X: input data
+    """
+    Sigma = np.cov(X, rowvar=0)
+    eigenvalue, eigenvector = np.linalg.eig(Sigma)
+    S = eigenvector
+    
+    return np.dot(S.T, X.T).T
+
+
+def whitening(X):
+    """whitening data.
+    
+    X: input data
+    """
+    X_centerized = X - X.mean(axis=0)
+    
+    Sigma = np.cov(X, rowvar=0)
+    eigenvalue, eigenvector = np.linalg.eig(Sigma)
+    S = eigenvector
+    
+    Lambda = np.dot(np.dot(np.linalg.inv(S), Sigma), S)
+    Lambda_sqrt_inv = np.linalg.inv(np.sqrt(Lambda))
+    
+    return np.dot(np.dot(X_centerized, S), Lambda_sqrt_inv.T)
+
